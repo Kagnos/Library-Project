fb8f2e795c202db4e1e0742f0b28f4e64cf7ef97
Kagnos <kagcodes@gmail.com>
2026-02-13 17:04:25 -0600

    Create new functions Create addBookToLibrary function that pushes a new book to the library array Remove theHobbit variable Add example books to library array Create callLibrary function that loops through the library array and displays each book Create library-container div to house callLibrary function display Style library-container to allow for line breaks

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-const library = [];
+const library = [
+  {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    pages: "295",
+    read: "have not",
+    id: crypto.randomUUID()
+  },
+  {
+    title: "Test Book 1",
+    author: "Edward Scissorhands",
+    pages: "351",
+    read: "have",
+    id: crypto.randomUUID()
+  },
+  {
+    title: "Test Book 2",
+    author: "Jacob Reinstein",
+    pages: "147",
+    read: "have not",
+    id: crypto.randomUUID()
+  }
+];
 
 function Book(title, author, pages, read) {
   if (!new.target) {
@@ -13,4 +35,22 @@ function Book(title, author, pages, read) {
   };
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "have not");
+function addBookToLibrary (title, author, pages, read) {
+  let newBook = {
+  title: title,
+  author: author,
+  pages: pages,
+  read: read,
+  id: crypto.randomUUID()
+  };
+  library.push(newBook);
+};
+
+const libraryContainer = document.querySelector("#library-container");
+
+function callLibrary() {
+  libraryContainer.textContent = "";
+  for (i = 0; i < library.length; i++) {
+    libraryContainer.textContent += `Title: ${library[i].title}\nAuthor: ${library[i].author}\nPages: ${library[i].pages}\nRead: ${library[i].read}\nid: ${library[i].id}\n\n`;
+  }
+};
