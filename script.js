d2a63531fa2123988410f51fd8e3011d09dedf68
Kagnos <kagcodes@gmail.com>
2026-02-17 18:21:10 -0600

    Update new book button and clean up code Detail new book button with form Create new book button form functionality and button functionalities Sync all tab spacing to 4 spaces Remove example button Restructure HTML Style sidebar and dialog button classes

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
 const library = [
-  {
-    title: "The Hobbit",
-    author: "J.R.R. Tolkien",
-    pages: "295",
-    read: "have not",
-    id: crypto.randomUUID()
-  },
-  {
-    title: "Test Book 1",
-    author: "Edward Scissorhands",
-    pages: "351",
-    read: "have",
-    id: crypto.randomUUID()
-  },
-  {
-    title: "Test Book 2",
-    author: "Jacob Reinstein",
-    pages: "147",
-    read: "have not",
-    id: crypto.randomUUID()
-  }
+    {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        pages: "295",
+        read: "have not",
+        id: crypto.randomUUID()
+    },
+    {
+        title: "Test Book 1",
+        author: "Edward Scissorhands",
+        pages: "351",
+        read: "have",
+        id: crypto.randomUUID()
+    },
+    {
+        title: "Test Book 2",
+        author: "Jacob Reinstein",
+        pages: "147",
+        read: "have not",
+        id: crypto.randomUUID()
+    }
 ];
 
 function Book(title, author, pages, read) {
-  if (!new.target) {
-    throw Error("You must use the 'new' operator to call the constructor");
-  }
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  id = crypto.randomUUID();
-  this.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, you ${this.read} read it before.`;
-  };
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    id = crypto.randomUUID();
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, you ${this.read} read it before.`;
+    };
 }
 
 function addBookToLibrary (title, author, pages, read) {
-  let newBook = {
-  title: title,
-  author: author,
-  pages: pages,
-  read: read,
-  id: crypto.randomUUID()
-  };
-  library.push(newBook);
+    let newBook = {
+        title: title,
+        author: author,
+        pages: pages,
+        read: read,
+        id: crypto.randomUUID()
+    };
+    library.push(newBook);
 };
 
 const libraryContainer = document.querySelector("#library-container");
@@ -56,9 +56,16 @@ function callLibrary() {
   }
 };
 
-const newBookButton = document.querySelector("#new-book-button");
+const allButtons = document.querySelectorAll(".button");
 const dialog = document.querySelector("dialog");
 
-newBookButton.addEventListener("click", () => {
-  dialog.showModal();
-});
+allButtons.forEach((button) => 
+    button.addEventListener("click", () => {
+        switch(button.id) {
+            case "new-book-button":
+                return dialog.showModal();
+            case "cancel-button":
+                return dialog.close();
+        }
+    })
+);
