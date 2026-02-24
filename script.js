f3b12586b33871f97f6060be562d67659a8d2218
Kagnos <kagcodes@gmail.com>
2026-02-24 13:46:04 -0600

    Fix submit buttons and clean up JavaScript Change add new book button and add username button types to submit instead of reset Create new event listeners for submit buttons with form reset method to clear form while requiring correct data Change some global variables to local variables Remove call library function Remove new book constructor function

--- script.js
+++ script.js
@@ -1,40 +1,16 @@
 const library = [];
 
-let div = document.createElement("div");
-
-let removeBookButton = document.createElement("button");
-removeBookButton.classList.add("button", "library-button", "remove-book-button");
-removeBookButton.textContent = "Remove";
-
-let placeholderBook = document.querySelector("#placeholder-book");
-
-const libraryContainer = document.querySelector("#library-container");
-
-let book = document.createElement("div")
-book.classList.add("book");
-
-const newBookDialogForm = document.querySelector("#new-book-dialog-form");
-
 const allButtons = document.querySelectorAll("button");
 
 const usernameDialog = document.querySelector("#username-dialog");
+const usernameDialogForm = document.querySelector("#username-dialog-form");
 const newBookDialog = document.querySelector("#new-book-dialog");
-
-function Book(title, author, pages, read) {
-    if (!new.target) {
-        throw Error("You must use the 'new' operator to call the constructor");
-    }
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    id = crypto.randomUUID();
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, you ${this.read} read it before.`;
-    };
-}
+const newBookDialogForm = document.querySelector("#new-book-dialog-form");
 
 function addBookToLibrary(title, author, pages, read) {
+    let placeholderBook = document.querySelector("#placeholder-book");
+    const libraryContainer = document.querySelector("#library-container");
+
     if(library.length === 0) {
         placeholderBook.remove();
     };
@@ -48,11 +24,11 @@ function addBookToLibrary(title, author, pages, read) {
     };
     library.push(newBook);
 
-    book = document.createElement("div");
+    let book = document.createElement("div");
     book.classList.add("book");
     libraryContainer.appendChild(book);
 
-    div = document.createElement("div");
+    let div = document.createElement("div");
     book.appendChild(div).textContent = `Title: ${newBook.title}`;
     div = document.createElement("div");
     book.appendChild(div).textContent = `Author: ${newBook.author}`;
@@ -61,7 +37,7 @@ function addBookToLibrary(title, author, pages, read) {
     div = document.createElement("div");
     book.appendChild(div).textContent = `Read: ${newBook.read}`;
 
-    removeBookButton = document.createElement("button");
+    let removeBookButton = document.createElement("button");
     removeBookButton.classList.add("library-button", "red-button");
     removeBookButton.id = newBook.id;
     removeBookButton.textContent = "Remove";
@@ -80,12 +56,6 @@ function addBookToLibrary(title, author, pages, read) {
 });
 };
 
-function callLibrary() {
-    for (i = 0; i < library.length; i++) {
-        libraryContainer.appendChild(book);
-    };
-};
-
 function createBook() {
     const data = new FormData(newBookDialogForm);
     const entries = Object.fromEntries(data.entries());
@@ -99,9 +69,9 @@ allButtons.forEach((button) =>
                 return newBookDialog.showModal();
             case "cancel-new-book-button":
                 return newBookDialog.close();
-            case "add-new-book-button":
-                createBook();
-                return newBookDialog.close();
+//            case "add-new-book-button":
+//                createBook();
+//                return newBookDialog.close();
             case "username-button":
                 return usernameDialog.showModal();
             case "cancel-username-button":
@@ -112,14 +82,24 @@ allButtons.forEach((button) =>
     })
 );
 
+usernameDialogForm.addEventListener("submit", () => {
+
+});
+
+newBookDialogForm.addEventListener("submit", () => {
+    createBook();
+    newBookDialog.close();
+    newBookDialogForm.reset();
+});
+
 
 // To Do:
 
-// Fix bug: forms aren't required anymore
+// Fix bug: forms aren't required anymore - DONE
 // Add view changer functionality
 // Add line breaks on books when they break container width
 // Add checkbox for read status that updates book text
-// Clean up js file, create headers
+// Clean up js file, create headers - DONE ish
 // Create functioning edit button
 
 // git message:
